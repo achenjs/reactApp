2beeafd5781236fdaa49932b4484059623205ffe
achenjs <hellowwc@aliyun.com>
2017-10-25 21:20:16 +0800

    合并

---
+++ src/components/List/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class List extends Component {
+  constructor (props, context) {
+    super(props, context);
+  }
+
+  render () {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
+
+export default List;
